78080aeeadef87145a4e48284970adc6c1e59d2b
sharad-vishe <sharad.vishe@dream11.com>
2025-11-20 18:54:35 +0530

    pre-commit changes

--- mysql/aws_k8s/discovery.sh
+++ mysql/aws_k8s/discovery.sh
@@ -3,22 +3,25 @@ set -euo pipefail
 
 DISCOVERY={}
 
+# shellcheck disable=SC1083
 export KUBECONFIG={{ componentMetadata.kubeConfigPath }}
+# shellcheck disable=SC1083
 export NAMESPACE={{ componentMetadata.envName }}
 RELEASE_NAME=$(jq -r '.releaseName' state.json)
 
 function get_endpoints() {
-  kubectl get endpointslices -n ${NAMESPACE} -l app.kubernetes.io/instance=${RELEASE_NAME},app.kubernetes.io/component=$1 | grep headless | awk '{gsub(":[0-9]+", "", $4); print $4}'
+  kubectl get endpointslices -n ${NAMESPACE} -l app.kubernetes.io/instance="${RELEASE_NAME}",app.kubernetes.io/component="$1" | grep headless | awk '{gsub(":[0-9]+", "", $4); print $4}'
 }
 
 WRITER_ENDPOINT=$(get_endpoints primary)
 
+# shellcheck disable=SC1083,SC1072,SC1073,SC1009
 if [[ {{ flavourConfig.reader.replicaCount  }} -eq 0 ]]; then
   READER_ENDPOINT=${WRITER_ENDPOINT}
 else
   READER_ENDPOINT=$(get_endpoints secondary)
 fi
 
-DISCOVERY=$(echo ${DISCOVERY} | jq -c --arg reader "$READER_ENDPOINT" --arg writer "$WRITER_ENDPOINT" '.reader=$reader | .writer=$writer')
+DISCOVERY=$(echo ${DISCOVERY} | jq -c --arg reader "${READER_ENDPOINT}" --arg writer "${WRITER_ENDPOINT}" '.reader=$reader | .writer=$writer')
 
-echo ${DISCOVERY}
+echo "${DISCOVERY}"
